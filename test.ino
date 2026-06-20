dcca08e5fbe24a478c936ef1b98e87fb5801cf65
Rakhaaaz <nurrakhmansyarifudin1@gmail.com>
2026-06-20 13:26:36 +0800

    tambah kontrol motor DC (forward/reverse/stop)
    - pake pin 9 (PWM) dan 8 (direction)
    - forward 2 detik, stop 1 detik, reverse 2 detik
    - pake analogWrite 150 buat test duty cycle

--- test.ino
+++ test.ino
@@ -1,14 +1,41 @@
 // test.ino - Motor driver test
 // Hallo test from Lihana VPS
 
+const int MOTOR_PIN = 9;
+const int DIR_PIN   = 8;
+
 void setup() {
   Serial.begin(9600);
   Serial.println("hallo");
   Serial.println("Motor driver test ready...");
+  
+  pinMode(MOTOR_PIN, OUTPUT);
+  pinMode(DIR_PIN, OUTPUT);
 }
 
 void loop() {
-  // Loop test
+  // Test hallo di loop
   Serial.println("hallo dari loop");
+  delay(2000);
+  
+  // Test motor forward
+  Serial.println("Motor FORWARD");
+  digitalWrite(DIR_PIN, HIGH);
+  analogWrite(MOTOR_PIN, 150);
+  delay(2000);
+  
+  // Stop
+  Serial.println("Motor STOP");
+  analogWrite(MOTOR_PIN, 0);
+  delay(1000);
+  
+  // Test motor reverse
+  Serial.println("Motor REVERSE");
+  digitalWrite(DIR_PIN, LOW);
+  analogWrite(MOTOR_PIN, 150);
+  delay(2000);
+  
+  // Stop
+  analogWrite(MOTOR_PIN, 0);
   delay(2000);
 }
