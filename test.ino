063650f3908f698356a92bd5bcd86faf8a0eaa04
Rakhaaaz <nurrakhmansyarifudin1@gmail.com>
2026-06-20 13:04:30 +0800

    test: hallo dari Lihana VPS

---
+++ test.ino
@@ -0,0 +1,12 @@
+// test.ino - First test commit from VPS
+// Motor driver test scaffold
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Hallo dari Lihana VPS!");
+  Serial.println("Ready for motor driver test...");
+}
+
+void loop() {
+  // Test code will go here
+}
